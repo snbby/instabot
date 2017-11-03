db5e6f3e5cfd49806db94d6bea102c7d019f5099
maksim.malyshev <maksim.malyshev@lamoda.ru>
2017-11-03 17:24:45 +0300

    + corrected command

--- bot/management/commands/run_old_bot.py
+++ bot/management/commands/run_old_bot.py
@@ -1,11 +1,38 @@
 from django.core.management import BaseCommand
 from django.conf import settings
 
-from old_bot.instabot import InstaBot
+from old_bot import InstaBot
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('-a', '--account', dest='account', type=str, choices=['test', 'radio'], required=True)
+        parser.add_argument('-t', '--tags', dest='tags', nargs='*', required=True)
+        parser.add_argument('-f', '--follow_per_day', dest='follow_per_day', type=int)
+        parser.add_argument('-l', '--like_per_day', dest='like_per_day', type=int)
+        parser.add_argument('--unfollow_per_day', dest='unfollow_per_day', type=int)
+        parser.add_argument('--media_max_like', dest='media_max_like', type=int)
+        parser.add_argument('--log_mode', dest='log_mod', choices=[0, 1, 2], type=int)
+
     def handle(self, *args, **options):
-        bot = InstaBot(settings.INSTA_USERNAME, settings.INSTA_PASSWORD,
-                       tag_list=['music', 'audio' 'cinema', 'songs', 'video', 'celebrity'])
+        username = None
+        password = None
+
+        if options['account'] == 'test':
+            username = settings.INSTA_USERNAME_TEST
+            password = settings.INSTA_PASSWORD_TEST
+        elif options['account'] == 'radio':
+            username = settings.INSTA_USERNAME
+            password = settings.INSTA_PASSWORD
+
+        bot = InstaBot(
+            username,
+            password,
+            tag_list=options['tags'],
+            follow_per_day=options.get('follow_per_day', 0),
+            like_per_day=options.get('like_per_day', 1000),
+            unfollow_per_day=options.get('unfollow_per_day', 0),
+            media_max_like=options.get('media_max_like', 0),
+            log_mod=options.get('log_mod', 0)
+        )
         bot.auto_mod()
--- bot/management/commands/test_command.py
+++ bot/management/commands/test_command.py
@@ -1,13 +1,14 @@
-
-import requests
 import logging
 from django.core.management import BaseCommand
 
 logger = logging.getLogger('bot.command.test_command')
 
+
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('-a', '--account', dest='account', type=str, choices=['test', 'radio'], required=True)
+        parser.add_argument('-t', '--tags', dest='tags', nargs='*', required=True)
+
     def handle(self, *args, **options):
-        requests.get('http://yandex.ru')
-        logger.debug('hohoh')
         print(args)
         print(options)
